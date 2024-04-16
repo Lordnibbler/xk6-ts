7d52d00dce145a80463e7b643e98755073f24a29
Ben Radler <ben.radler@getcruise.com>
2024-04-16 14:08:18 -0700

    try

--- loader.go
+++ loader.go
@@ -3,6 +3,7 @@ package ts
 import (
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -50,7 +51,6 @@ func redirectStdin() {
 	source, err := os.ReadFile(filepath.Clean(filename))
 	if err != nil {
 		logrus.WithError(err).Fatal()
-		return
 	}
 
 	packStarted := time.Now()
@@ -58,7 +58,6 @@ func redirectStdin() {
 	jsScript, err := k6pack.Pack(string(source), opts)
 	if err != nil {
 		logrus.WithError(err).Fatal()
-		return
 	}
 
 	if os.Getenv("XK6_TS_BENCHMARK") == "true" {
@@ -66,38 +65,33 @@ func redirectStdin() {
 		logrus.WithField("extension", "xk6-ts").WithField("duration", duration).Info("Bundling completed in ", duration)
 	}
 
-	logrus.WithField("extension", "xk6-ts").Info("os.Args", os.Args)
-
-	os.Args[scriptIndex] = "-" // Set this so k6 reads from stdin
-
 	reader, writer, err := os.Pipe()
 	if err != nil {
 		logrus.WithError(err).Fatal()
 		return
 	}
 
-	// var wg sync.WaitGroup
-	// wg.Add(1)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Start a goroutine to handle the writing to the pipe
+	go func() {
+		defer wg.Done()
+		defer writer.Close() // Close writer after writing to signal EOF
+		if _, err := writer.Write(jsScript); err != nil {
+			logrus.WithError(err).Error("Failed to write JS script to pipe")
+		}
+	}()
 
 	// Replace os.Stdin with the read end of the pipe
 	origStdin := os.Stdin
 	os.Stdin = reader
-
-	// like a finally
-	// defer func() {
-	// 	os.Stdin = origStdin
-	// 	// reader.Close()
-	// }()
-
-	// Start a goroutine to handle the writing to the pipe
-	// go func() {
-	// 	defer wg.Done()
-	defer writer.Close() // Close writer after writing to signal EOF
-	if _, err := writer.Write(jsScript); err != nil {
-		logrus.WithError(err).Error("Failed to write JS script to pipe")
+	defer func() {
 		os.Stdin = origStdin
-	}
-	// }()
-	// wg.Wait() // Wait for writing to complete before proceeding
+		reader.Close()
+	}()
 
+	wg.Wait() // Wait for writing to complete before proceeding
+
+	os.Args[scriptIndex] = "-" // Set this so k6 reads from stdin
 }
